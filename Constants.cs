96acb72d3e03c39bb6c6fa795c29ed9fa8d85c00
Gustave M <gus33000@live.fr>
2018-12-09 21:58:55 +0100

    Initial Commit

---
+++ Constants.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) 2018, Gustave M. - gus33000.me - @gus33000
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+namespace DumpIt
+{
+    internal class Constants
+    {
+        internal const ulong SectorSize = 0x200;
+
+        internal readonly static string[] partitions = new string[]
+        {
+            "DPP",
+            "MODEM_FSG",
+            "MODEM_FS1",
+            "MODEM_FS2",
+            "MODEM_FSC",
+            "DDR",
+            "SEC",
+            "APDP",
+            "MSADP",
+            "DPO",
+            "SSD",
+            "UEFI_BS_NV",
+            "UEFI_NV",
+            "UEFI_RT_NV",
+            "UEFI_RT_NV_RPMB",
+            "BOOTMODE",
+            "LIMITS",
+            "BACKUP_BS_NV",
+            "BACKUP_SBL1",
+            "BACKUP_SBL2",
+            "BACKUP_SBL3",
+            "BACKUP_PMIC",
+            "BACKUP_DBI",
+            "BACKUP_UEFI",
+            "BACKUP_RPM",
+            "BACKUP_QSEE",
+            "BACKUP_QHEE",
+            "BACKUP_TZ",
+            "BACKUP_HYP",
+            "BACKUP_WINSECAPP",
+            "BACKUP_TZAPPS",
+            "SVRawDump"
+        };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,176 @@
+﻿// Copyright (c) 2018, Gustave M. - gus33000.me - @gus33000
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+using CommandLine;
+using CommandLine.Text;
+using DiscUtils;
+using DiscUtils.Containers;
+using DiscUtils.Raw;
+using DiscUtils.Streams;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace DumpIt
+{
+    class Program
+    {
+        internal static string[] partitions = Constants.partitions;
+
+        static void Main(string[] args)
+        {
+            var ass = Assembly.GetExecutingAssembly();
+            var fvi = FileVersionInfo.GetVersionInfo(ass.Location);
+            var Heading = new HeadingInfo(fvi.FileDescription, ass.GetName().Version.ToString());
+            var Copyright = new CopyrightInfo(fvi.CompanyName, DateTime.Today.Year);
+
+            Parser.Default.ParseArguments<Options>(args).WithParsed(o =>
+            {
+                Console.WriteLine(Heading.ToString());
+                Console.WriteLine(Copyright.ToString());
+                Console.WriteLine();
+                
+                if (!string.IsNullOrEmpty(o.Excludelist) && File.Exists(o.Excludelist))
+                    partitions = new List<string>(File.ReadAllLines(o.Excludelist)).ToArray();
+                
+                ulong eMMCDumpSize;
+                ulong SectorSize = 0x200;
+
+                if (o.ImgFile.ToLower().Contains(@"\\.\physicaldrive"))
+                {
+                    Logging.Log("Tool is running in Device Dump mode.");
+                    Logging.Log("Gathering disk geometry...");
+                    eMMCDumpSize = (ulong)GetDiskSize.GetDiskLength(@"\\.\PhysicalDrive" + o.ImgFile.ToLower().Replace(@"\\.\physicaldrive", ""));
+                    SectorSize = (ulong)GetDiskSize.GetDiskSectorSize(@"\\.\PhysicalDrive" + o.ImgFile.ToLower().Replace(@"\\.\physicaldrive", ""));
+                }
+                else
+                {
+                    Logging.Log("Tool is running in Image Dump mode.");
+                    Logging.Log("Gathering disk image geometry...");
+                    eMMCDumpSize = (ulong)new FileInfo(o.ImgFile).Length;
+                }
+
+                Logging.Log("Reported source device eMMC size is: " + eMMCDumpSize + " bytes - " + eMMCDumpSize / 1024 / 1024 + "MB - " + eMMCDumpSize / 1024 / 1024 / 1024 + "GB.");
+                Logging.Log("Selected " + SectorSize + "B for the sector size");
+                
+                ConvertDD2VHD(o.ImgFile, o.VhdFile, partitions, o.Recovery, (int)SectorSize);
+            });
+        }
+
+        /// <summary>
+        ///     Coverts a raw DD image into a VHD file suitable for FFU imaging.
+        /// </summary>
+        /// <param name="ddfile">The path to the DD file.</param>
+        /// <param name="vhdfile">The path to the output VHD file.</param>
+        /// <returns></returns>
+        public static void ConvertDD2VHD(string ddfile, string vhdfile, string[] partitions, bool Recovery, int SectorSize)
+        {
+            SetupHelper.SetupContainers();
+            Stream strm;
+
+            if (ddfile.ToLower().Contains(@"\\.\physicaldrive"))
+                strm = new DeviceStream(ddfile);
+            else
+                strm = new FileStream(ddfile, FileMode.Open);
+
+            Stream fstream;
+            if (!Recovery)
+                fstream = new EPartitionStream(strm, partitions);
+            else
+                fstream = strm;
+
+            using (var inDisk = new Disk(fstream, Ownership.Dispose))
+            {
+                var diskParams = inDisk.Parameters;
+
+                using (var outDisk = VirtualDisk.CreateDisk("VHD", "dynamic", vhdfile, diskParams, "", ""))
+                {
+                    var contentStream = inDisk.Content;
+
+                    var pump = new StreamPump
+                    {
+                        InputStream = contentStream,
+                        OutputStream = outDisk.Content,
+                        SparseCopy = true,
+                        SparseChunkSize = SectorSize,
+                        BufferSize = SectorSize * 1024
+                    };
+
+                    var totalBytes = contentStream.Length;
+
+                    var now = DateTime.Now;
+                    pump.ProgressEvent += (o, e) => { ShowProgress(totalBytes, now, o, e); };
+
+                    Logging.Log("Converting RAW to VHD");
+                    pump.Run();
+                    Console.WriteLine();
+                }
+            }
+        }
+        
+        protected static void ShowProgress(long totalBytes, DateTime startTime, object sourceObject,
+            PumpProgressEventArgs e)
+        {
+            var now = DateTime.Now;
+            var timeSoFar = now - startTime;
+
+            var remaining =
+                TimeSpan.FromMilliseconds(timeSoFar.TotalMilliseconds / e.BytesRead * (totalBytes - e.BytesRead));
+
+            var speed = Math.Round(e.SourcePosition / 1024L / 1024L / timeSoFar.TotalSeconds);
+
+            Logging.Log(
+                string.Format("{0} {1}MB/s {2:hh\\:mm\\:ss\\.f}", GetDismLikeProgBar((int)(e.BytesRead * 100 / totalBytes)), speed.ToString(),
+                    remaining, remaining.TotalHours, remaining.Minutes, remaining.Seconds, remaining.Milliseconds),
+                returnline: false);
+
+        }
+
+        private static string GetDismLikeProgBar(int perc)
+        {
+            var eqsLength = (int)((double)perc / 100 * 55);
+            var bases = new string('=', eqsLength) + new string(' ', 55 - eqsLength);
+            bases = bases.Insert(28, perc + "%");
+            if (perc == 100)
+                bases = bases.Substring(1);
+            else if (perc < 10)
+                bases = bases.Insert(28, " ");
+            return "[" + bases + "]";
+        }
+        
+        internal class Options
+        {
+            [Option('i', "img-file", HelpText = @"A path to the img file to convert *OR* a PhysicalDisk path. i.e. \\.\PhysicalDrive1", Required = true)]
+            public string ImgFile { get; set; }
+
+            [Option('v', "vhd-file", HelpText = "A path to the VHD file to output", Required = true)]
+            public string VhdFile { get; set; }
+            
+            [Option('e', "exclude-list", Required = false,
+                HelpText = "Path to an optional partition exclude text list to use instead of the builtin one.")]
+            public string Excludelist { get; set; }
+            
+            [Option('r', "enable-recoveryvhd", Required = false, HelpText = "Generates a recovery vhd with no partition skipped. Useful for clean state restore for a SPECIFIC unique device.", Default = false)]
+            public bool Recovery { get; set; }
+        }
+    }
+}
